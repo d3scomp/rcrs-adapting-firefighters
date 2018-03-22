0479260a358c8bb2dc3d69673579fc22028865f5
Dominik Skoda <skoda@d3s.mff.cuni.cz>
2018-03-22 14:41:54 +0100

    ng bugs in BatchPlot.sh

--- scripts/BatchPlot.sh
+++ scripts/BatchPlot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Adjust the number of simulation groups
-G_CNT = 6
+G_CNT=6
 # Adjust the number of simulations in a groups
-SUB = 8
+SUB=8
 
-for i in $(seq 0 $((${G_CNT-1))); do
+for i in $(seq 0 $((${G_CNT}-1))); do
   python Plot.py $(seq $(($i*${SUB})) $((($i+1)*${SUB}-1)));
   echo "Plot $i" > ../results/figures/description_$i.txt;
   for j in $(seq $(($i*${SUB})) $((($i+1)*${SUB}-1))); do
