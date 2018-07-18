ea1fdbee0cc8a486f21fe75ffe4bb37a8643e597
Dominik Skoda <skoda@carbon.d3s.hide.ms.mff.cuni.cz>
2018-07-18 11:43:13 +0200

    Improved batch plotting

--- scripts/BatchPlot.sh
+++ scripts/BatchPlot.sh
@@ -4,11 +4,14 @@
 G_CNT=6
 # Adjust the number of simulations in a groups
 SUB=8
+OFFSET=2
 
 for i in $(seq 0 $((${G_CNT}-1))); do
-  python Plot.py $(seq $(($i*${SUB})) $((($i+1)*${SUB}-1)));
+  from=`echo $OFFSET+$i*${SUB} | bc`
+  to=`echo $OFFSET+\($i+1\)*${SUB}-1 | bc`
+  python Plot.py $(seq $from $to);
   echo "Plot $i" > ../results/figures/description_$i.txt;
-  for j in $(seq $(($i*${SUB})) $((($i+1)*${SUB}-1))); do
+  for j in $(seq $from $to); do
     echo $j >> ../results/figures/description_$i.txt;
     cat ../results/logs/scenario_$j/description.txt >> ../results/figures/description_$i.txt;
   done
